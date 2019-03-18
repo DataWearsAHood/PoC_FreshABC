ee642e8e5df52597423cfcb317f1d9a946a077f5
Adam Smooch <adam@smooch.io>
2019-03-18 11:31:22 -0400

    moved to WIP

--- tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-from __future__ import print_function
-from Crypto.Cipher import AES
-from Crypto.Util import Counter
-from Crypto import Random
-
-random_generator = Random.new()
-IV = random_generator.read(8)
-print(IV)
